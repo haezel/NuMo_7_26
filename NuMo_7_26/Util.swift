0ab3d254e727aeb9054edb0ece9e7a58680ffe43
Kathryn Manning <manningkathryn@gmail.com>
2015-10-26 15:10:08 -0600

    updated database filepath creation in util file

--- NuMo_7_26/Util.swift
+++ NuMo_7_26/Util.swift
@@ -6,22 +6,31 @@ class Util: NSObject {
     class func getPath(fileName: String) -> String
     {
         
-        //return NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)[0].stringByAppendingPathComponent(fileName)
+        let documentsURL = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)[0]
+        let fileURL = documentsURL.URLByAppendingPathComponent(fileName)
+        return fileURL.path!
         
-        var v = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
-     
-        let x = v[0]
-        
-        let z = NSURL(fileURLWithPath: x).URLByAppendingPathComponent(fileName)
-        
-        return String(z)
-        
-        
-        
-//          examplel 1.2 to 2 swift
-//        let writePath = NSTemporaryDirectory().stringByAppendingPathComponent("instagram.igo")
 //        
-//        let writePath = NSURL(fileURLWithPath: NSTemporaryDirectory()).URLByAppendingPathComponent("instagram.igo")
+//        
+//        
+//        
+//        
+//        //return NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)[0].stringByAppendingPathComponent(fileName)
+//        
+//        var v = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+//     
+//        let x = v[0]
+//        
+//        let z = NSURL(fileURLWithPath: x).URLByAppendingPathComponent(fileName)
+//        
+//        return String(z)
+//        
+//        
+//        
+////          examplel 1.2 to 2 swift
+////        let writePath = NSTemporaryDirectory().stringByAppendingPathComponent("instagram.igo")
+////        
+////        let writePath = NSURL(fileURLWithPath: NSTemporaryDirectory()).URLByAppendingPathComponent("instagram.igo")
         
         
     }
@@ -29,18 +38,58 @@ class Util: NSObject {
     //copy databse file .sqlite from project location to device location
     class func copyFile(fileName : NSString)
     {
-        let dbPath : NSString = getPath(fileName as String)
+        let dbPath: String = getPath(fileName as String)
         let fileManager = NSFileManager.defaultManager()
-        if !fileManager.fileExistsAtPath(dbPath as String)
-        {
-            print("File didnt already exist")
-            let fromPath : String? = (NSBundle.mainBundle().resourcePath! as NSString).stringByAppendingPathComponent(fileName as String)
+        if !fileManager.fileExistsAtPath(dbPath) {
+            let documentsURL = NSBundle.mainBundle().resourceURL
+            let fromPath = documentsURL!.URLByAppendingPathComponent(fileName as String)
+            var error : NSError?
             do {
-                try fileManager.copyItemAtPath(fromPath!, toPath: dbPath as String)
-            } catch _ {
+                try fileManager.copyItemAtPath(fromPath.path!, toPath: dbPath)
+            } catch let error1 as NSError {
+                error = error1
             }
+            let alert: UIAlertView = UIAlertView()
+            if (error != nil) {
+                alert.title = "Error Occured"
+                alert.message = error?.localizedDescription
+            } else {
+                alert.title = "Successfully Copy"
+                alert.message = "Your database copy successfully"
+            }
+            alert.delegate = nil
+            alert.addButtonWithTitle("Ok")
+            alert.show()
         }
-        else{print("File already existed")}
+        
+        
+        
+        
+//        
+//        let dbPath : NSString = getPath(fileName as String)
+//        let fileManager = NSFileManager.defaultManager()
+//        if !fileManager.fileExistsAtPath(dbPath as String)
+//        {
+//            print("File didnt already exist")
+//            let fromPath : String? = (NSBundle.mainBundle().resourcePath! as NSString).stringByAppendingPathComponent(fileName as String)
+//            do {
+//                try fileManager.copyItemAtPath(fromPath!, toPath: dbPath as String)
+//            } catch _ {
+//            }
+//        }
+//        else{print("File already existed")}
+    }
+    
+    
+    
+    class func invokeAlertMethod(strTitle: NSString, strBody: NSString, delegate: AnyObject?)
+    {
+        let alert: UIAlertView = UIAlertView()
+        alert.message = strBody as String
+        alert.title = strTitle as String
+        alert.delegate = delegate
+        alert.addButtonWithTitle("Ok")
+        alert.show()
     }
 
 }
